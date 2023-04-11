10e13405f3cfb3484cd8918ef84fa95379d89f6d
shocyu <sanokebab@gmail.com>
2023-04-11 20:53:50 +0900

    update plus and minus calculation

--- scc.c
+++ scc.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define D_PRINT_TAB printf("  ");
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "number of arguments is not correct.\n");
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".intel_syntax noprefix\n");
   printf(".globl _main\n");
   printf("_main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  D_PRINT_TAB
+  printf("mov rax, %ld\n", strtol(p, &p, 10));
+
+  while(*p){
+    if(*p == '+'){
+      p++;
+      D_PRINT_TAB
+      printf("add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if(*p == '-'){
+      p++;
+      D_PRINT_TAB
+      printf("sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "error: unexpected input '%c'\n", *p);
+    return 1;
+
+  }
+
   printf("  ret\n");
   return 0;
 }
